439f70613d828c625cb9c5f01a013340e06950a0
mtema <mario.tema@abas.de>
2019-01-30 16:47:51 +0100

    compile to commonjs

--- src/index.ts
+++ src/index.ts
@@ -4,56 +4,56 @@
  * M meta generic type
  */
 export class ModelConverter<S, T, M> {
-  /**
-   * Convert source to target.
-   * If source is null or undefined
-   * return null.
-   *
-   * @param source may null or undefined
-   * @param meta may null or undefined
-   * @return new instance of target or null
-   */
-  public convert(source: S, meta?: M): T | null {
-    if (!source) {
-      return null;
+    /**
+     * Convert source to target.
+     * If source is null or undefined
+     * return null.
+     *
+     * @param source may null or undefined
+     * @param meta may null or undefined
+     * @return new instance of target or null
+     */
+    public convert(source: S, meta?: M): T | null {
+        if (!source) {
+            return null;
+        }
+
+        const target = {} as T;
+
+        this.update(target, source, meta);
+
+        return target;
     }
 
-    const target = {} as T;
+    /**
+     *
+     * Copy the values of all of the enumerable own properties from one or more source objects to a
+     * target object.
+     *
+     * @param target may null or undefined
+     * @param source may null or undefined
+     * @param meta may null or undefined
+     */
+    public update(target: T | any, source: S, meta?: M): void {
+        if (!source) {
+            return;
+        }
 
-    this.update(target, source, meta);
-
-    return target;
-  }
-
-  /**
-   *
-   * Copy the values of all of the enumerable own properties from one or more source objects to a
-   * target object.
-   *
-   * @param target may null or undefined
-   * @param source may null or undefined
-   * @param meta may null or undefined
-   */
-  public update(target: T | null, source: S, meta?: M): void {
-    if (!source) {
-      return;
+        Object.keys(source).forEach(it => target[it] = (source as any)[it]);
     }
 
-    Object.assign(target, source);
-  }
+    /**
+     *
+     * @param sources may null or undefined
+     * @param meta may null or undefined
+     *
+     * @return Array never null
+     */
+    public convertList(sources: S[], meta?: M): T[] {
+        if (sources === null || sources === undefined || sources.length === 0) {
+            return [];
+        }
 
-  /**
-   *
-   * @param sources may null or undefined
-   * @param meta may null or undefined
-   *
-   * @return Array never null
-   */
-  public convertList(sources: S[], meta?: M): T[] {
-    if (sources === null || sources === undefined || sources.length === 0) {
-      return [];
+        return sources.map(it => this.convert(it, meta)) as T[];
     }
-
-    return sources.map(it => this.convert(it, meta)) as T[];
-  }
 }
